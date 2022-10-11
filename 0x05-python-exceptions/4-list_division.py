8d271ae54bfe6334ee48c7f3e0d342c8aae796d1
beginners-lock <99103645+beginners-lock@users.noreply.github.com>
2022-10-11 05:43:56 +0100

    first

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides two lists element by element.
-    args:
-        my_list_1 (list) - The first list.
-        my_list_2 (list) - The second list.
-        list_length (int) - The number of elements to divide.
-    returns:
-       a new list of length list_length containing all the divisions.
-    """
+    """Divides two lists element by element."""
     new_list = []
     for index in range(0, list_length):
         try:
             div = my_list_1[index] / my_list_2[index]
         except TypeError:
-            print("DataType Error")
+            print("wrong type")
             div = 0
         except ZeroDivisionError:
-            print("Zero Divison Error")
+            print("division by 0")
             div = 0
         except IndexError:
-            print("Invalid Index")
+            print("out of range")
             div = 0
         finally:
             new_list.append(div)
